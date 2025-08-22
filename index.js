43d6b3ee9c8480a89be9fd36bb06309d215e6b30
Misskhat <misskhat.hossen@gmail.com>
2025-08-22 21:30:49 +0600

    add event listener with login button and forward to app home page

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('object');
+document.getElementById('loginBtn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    // Statically declare an user mobile number and pin number for validation with user input.
+    const storeMobileNumber = 12345678910;
+    const storePinNumber = 1234;
+
+    // User input collect and converted to integer;
+    const userInputMobileNumber = document.getElementById('mobile-number').value;
+    const userInputMobileNumberConverted = parseInt(userInputMobileNumber)
+    
+    const userInputPinNumber = document.getElementById('pin-number').value;
+    const userInputPinNumberConverted = parseInt(userInputPinNumber)
+
+    if (storeMobileNumber === userInputMobileNumberConverted && storePinNumber === userInputPinNumberConverted) {
+        window.location.href='./home.html'
+    } else {
+        alert("Invalid credentials")
+    }
+})
